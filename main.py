4802262aa1f396fe33a819a044358c8c5969d7ba
Cicero Henrique <cicero_penha@hotmail.com>
2021-06-30 19:07:05 -0300

    Reduce the code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import glob
-import os.path
 import os
 
 EXTENSIONS = [
@@ -13,42 +12,41 @@ EXTENSIONS = [
     'flv'
 ]
 
-def goDeep(files):
-    for file in files:
-        if isDir(file):
-            search_in_folder()
-
-def isValidUrl(url):
+def validateFolderName(original_url):
+    url = original_url
     for letter in url:
         if letter == '[':
             url = url.replace(letter, '(')
         if letter == ']':
             url = url.replace(letter, ')')
+    if url != original_url:
+        os.rename(original_url, url)
     return url
 
 def isDir(path):
     return os.path.isdir(path)
 
-def search_videos(url):
+def search_videos(url, dashes):
     names = []
     for extension in EXTENSIONS:
         videos = glob.glob(url + "\\*." + extension)
         for x in videos:
-            names.append('- ' + str(x.replace(url+'\\', '').replace('.' + extension, '')))
+            names.append(dashes + '- ' + str(x.replace(url+'\\', '').replace('.' + extension, '')))
+    if len(names) != 0:
+        names = [dashes + ' ' + url] + names
     return names
 
-def search_in_folder(old_url, videos):
-    url = isValidUrl(old_url)
-    if url != old_url:
-        os.rename(old_url, url)
-    videos = videos + search_videos(url)
+def search_in_folder(old_url, videos, dashes):
+    url = validateFolderName(old_url)
+    videos = videos + search_videos(url, dashes)
+    dashes = dashes + '-'
     all_files = glob.glob(url+ "\*")
     for file in all_files:
         if isDir(file):
-            videos = videos + search_in_folder(file, [])
+            videos = videos + search_in_folder(file, [], dashes)
     return videos
 
 url = ''
-complete_list = search_in_folder(url, [])
+complete_list = search_in_folder(url, [], '-')
 for i in complete_list:
     print(i)
